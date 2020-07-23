c2bab75355792ed1fe964903f64bf23d030385dd
chandanaRobba <68033791+chandanaRobba@users.noreply.github.com>
2020-07-23 11:01:33 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -220,6 +220,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.coins, function (sprite, otherSp
     info.changeScoreBy(1)
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.pewPew.play()
     Present2 = sprites.create(img`
 . 2 2 2 . . . . 
 f f f f f f f f 
@@ -252,6 +253,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 sprites.onOverlap(SpriteKind.Player, SpriteKind.life, function (sprite, otherSprite) {
     otherSprite.destroy()
     info.changeLifeBy(1)
+    music.magicWand.play()
 })
 scene.onHitWall(SpriteKind.Projectile, function (sprite) {
     sprite.destroy()
